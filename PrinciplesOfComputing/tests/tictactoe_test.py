e0406a135cdc479c9a12e6f4186325c84dabb562
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2020-08-21 17:54:41 +0300

    Add test for score updating

--- PrinciplesOfComputing/tests/tictactoe_test.py
+++ PrinciplesOfComputing/tests/tictactoe_test.py
@@ -22,6 +22,44 @@ class Test2048Game(unittest.TestCase):
         self.assertIsNotNone(board.check_win())
         self.assertIn(board.check_win(), [provided.PLAYERO, provided.PLAYERX, provided.DRAW])
 
+    def test_update_scores_player_won(self):
+        dim = 3
+        board = [
+            [provided.PLAYERO, provided.PLAYERX, provided.PLAYERO],
+            [provided.PLAYERX, provided.PLAYERO, provided.PLAYERX],
+            [provided.EMPTY, provided.PLAYERX, provided.PLAYERO]
+        ]
+        expected = [[0 for _ in range(dim)] for _ in range(dim)]
+        for row in range(dim):
+            for col, square in enumerate(board[row]):
+                if square == provided.PLAYERO:
+                    expected[row][col] += tictactoe.SCORE_CURRENT
+                if square == provided.PLAYERX:
+                    expected[row][col] -= tictactoe.SCORE_OTHER
+        board = provided.TTTBoard(dim, False, board)
+        scores = [[0 for _ in range(dim)] for _ in range(dim)]
+        tictactoe.mc_update_scores(scores, board, provided.PLAYERO)
+        self.assertEqual(expected, scores)
+
+    def test_update_scores_player_lost(self):
+        dim = 3
+        board = [
+            [provided.PLAYERO, provided.PLAYERX, provided.PLAYERO],
+            [provided.PLAYERX, provided.PLAYERO, provided.PLAYERX],
+            [provided.EMPTY, provided.PLAYERX, provided.PLAYERO]
+        ]
+        expected = [[0 for _ in range(dim)] for _ in range(dim)]
+        for row in range(dim):
+            for col, square in enumerate(board[row]):
+                if square == provided.PLAYERO:
+                    expected[row][col] -= tictactoe.SCORE_CURRENT
+                if square == provided.PLAYERX:
+                    expected[row][col] += tictactoe.SCORE_OTHER
+        board = provided.TTTBoard(dim, False, board)
+        scores = [[0 for _ in range(dim)] for _ in range(dim)]
+        tictactoe.mc_update_scores(scores, board, provided.PLAYERO)
+        self.assertEqual(expected, scores)
+
 
 def run_suite(game_class):
     """
