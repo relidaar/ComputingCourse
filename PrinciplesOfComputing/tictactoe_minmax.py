73afa549c64992669570e301c2ffa11a245c414c
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2020-09-28 16:17:11 +0300

    Implement minmax search

--- PrinciplesOfComputing/tictactoe_minmax.py
+++ PrinciplesOfComputing/tictactoe_minmax.py
@@ -1,15 +1,19 @@
 """
 Mini-max Tic-Tac-Toe Player
 """
+import random
+import poc_ttt_gui
+import poc_ttt_provided as provided
+import codeskulptor
 
-try:
-    import poc_ttt_gui
-    import poc_ttt_provided as provided
-    import codeskulptor
-except ImportError:
-    from libs import poc_ttt_gui
-    import libs.poc_ttt_provided as provided
-    from SimpleGUICS2Pygame import codeskulptor
+# try:
+#     import poc_ttt_gui
+#     import poc_ttt_provided as provided
+#     import codeskulptor
+# except ImportError:
+#     from libs import poc_ttt_gui
+#     import libs.poc_ttt_provided as provided
+#     from SimpleGUICS2Pygame import codeskulptor
 
 # Set timeout, as mini-max can take a long time
 codeskulptor.set_timeout(60)
@@ -28,7 +32,19 @@ def mm_move(board, player):
     of the given board and the second element is the desired move as a
     tuple, (row, col).
     """
-    return 0, (-1, -1)
+    game_result = board.check_win()
+    if game_result:
+        return SCORES[game_result], (-1, -1)
+
+    moves = dict()
+    for move in board.get_empty_squares():
+        current_board = board.clone()
+        current_board.move(move[0], move[1], player)
+        result = mm_move(current_board, provided.PLAYERX if player == provided.PLAYERO else provided.PLAYERO)
+        moves[move] = result[0]
+
+    best_move = max(moves.items(), key=lambda item: item[1] * SCORES[player])
+    return best_move[1], best_move[0]
 
 
 def move_wrapper(board, player, trials):
